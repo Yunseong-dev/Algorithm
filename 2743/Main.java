4b50c40c71e11a3bd7a0422c5b29d2fffa51d8b5
Yunseong Jeong <jyerd333@gmail.com>
2023-11-01 15:16:41 +0900

    add

---
+++ 2743/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String n = scanner.nextLine();
+        System.out.println(n.length());
+
+        scanner.close();
+    }
+}
---
+++ 27866/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+        int i = scanner.nextInt() - 1;
+
+        System.out.println(str.charAt(i));
+        scanner.close();
+    }
+}
